52994e9c9dfedf94e15fdfe6b89a80395e5dff69
Andrew Delapaz <ardelapaz@yahoo.com>
2018-03-25 13:22:08 -0400

    Finished CSV Checkpoint

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook 
     attr_reader :entries
@@ -44,5 +45,14 @@ class AddressBook
 
     def amount #determine how many elements in the array for invalid entry testing
         entries.length
+    end
+
+    def import_from_csv(file_name)
+        csv_text = File.read(file_name)
+        csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+        csv.each do |row|
+            row_hash = row.to_hash
+            add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+        end
     end
 end
