cb6202f6c962421edaf6d30c5dac45fe0ad6bd36
Andrew Delapaz <ardelapaz@yahoo.com>
2018-03-24 22:29:42 -0400

    Remove entry complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,18 @@ class AddressBook
         end
         entries.insert(index, Entry.new(name, phone_number, email))
     end
+
+    def remove_entry(name, phone_number, email) 
+        deleted = nil
+        entries.each do |entry|
+            if name == entry.name
+                if phone_number == entry.phone_number
+                    if email == entry.email
+                        deleted = entry
+                    end
+                end
+            end
+        end
+        entries.delete(deleted)
+    end
 end
